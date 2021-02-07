d9318eceff0159d4366a9a404786e9d0396a9705
caroger <rogerhu1989@gmail.com>
2021-02-06 17:23:11 -0800

    react-tutorial

---
+++ W10D2/react-tutorial/src/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+
+//Conditional Rendering
+
+// function UserGreeting(props) {
+//   return <h1>Welcome back!</h1>;
+// }
+
+// function GuestGreeting(props) {
+//   return <h1>Please sign up.</h1>;
+// }
+
+// function Greeting(props) {
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+
+// function LoginButton(props) {
+//   return <button onClick={props.onClick}>Login</button>;
+// }
+
+// function LogoutButton(props) {
+//   return <button onClick={props.onClick}>Logout</button>;
+// }
+
+// class LoginControl extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = { isLoggedIn: false };
+//   }
+
+//   handleLoginClick() {
+//     this.setState({ isLoggedIn: true });
+//   }
+//   handleLogoutClick() {
+//     this.setState({ isLoggedIn: false });
+//   }
+
+//   render() {
+//     const isLoggedIn = this.state.isLoggedIn;
+//     let button;
+//     if (isLoggedIn) {
+//       button = <LogoutButton onClick={this.handleLogoutClick} />;
+//     } else {
+//       button = <LoginButton onClick={this.handleLoginClick} />;
+//     }
+
+//     return (
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         {button}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<LoginControl />, document.getElementById("root"));
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 && (
+        <h2>You have {unreadMessages.length} unread messages.</h2>
+      )}
+    </div>
+  );
+}
+
+const messages = ["React", "Re: React", "Re:Re: React"];
+ReactDOM.render(
+  <Mailbox unreadMessages={messages} />,
+  document.getElementById("root")
+);
